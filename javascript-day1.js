85bdb0c7c9bb4606469a1ccd68b0d406c6c6fe98
Code-with-Rudro <rudroprasadpanigrahi@gmail.com>
2026-02-22 01:09:02 +0530

    javascript day 1

---
+++ javascript-day1.js
@@ -0,0 +1,17 @@
+// DAY-1
+// object (non-primitive data type).
+const portfolio ={
+    accName : "Rudro prasad panigrahi",
+    follow : "FOLLOW",
+    dm : "message",
+    post : 196 + "post",
+    folling : 29 + "k",
+    folowers : 1,
+    discription : "yo its my ploser to connect with you",
+    location : "ongole,AP",
+}
+console.log(portfolio)
+ 
+//DAY-2
+
+
