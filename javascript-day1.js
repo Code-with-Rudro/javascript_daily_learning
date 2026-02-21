56ae31e05a727b92e3fe1594885897e19519d948
Code-with-Rudro <rudroprasadpanigrahi@gmail.com>
2026-02-22 02:05:17 +0530

    javascript day 1 operators

--- javascript-day1.js
+++ javascript-day1.js
@@ -14,4 +14,54 @@ console.log(portfolio)
  
 //DAY-2
 
+//ARTHIMETIC OPERATOR 
+
+let num_a = 10;
+let num_b = 2;
+
+console.log("a+b =", num_a+num_b);
+console.log("a-b =", num_a-num_b);
+console.log("a*b =", num_a*num_b);
+console.log("a/b =", num_a/num_b);
+console.log("a%b =", num_a%num_b);
+console.log("a**b =", num_a**num_b);
+
+//ASSIGNMENT OPERATOR 
+
+let X = 20;
+let y = 4;
+
+console.log("X=y =", X=y);
+console.log("X+=y =", X+=y);
+console.log("X-=y =", X-=y);
+console.log("X*=y =", X*=y);
+
+//COMPARISON OPERATOR 
+
+let num = 10;
+let num1 = 5;
+let num2 = "10";
+
+console.log("num==num1 =", num==num1);
+console.log("num===num2", num===num2);
+console.log("num===num2", num===num1);
+console.log("num!=num1", num != num1);
+console.log("num===num2", num !== num1);
+console.log("num>num1", num>num1);
+console.log("num<num1", num<num1);
+console.log("num>=num1", num>=num1);
+
+//LOGICAL OPERATOR
+
+let kiran = 10;
+let rudra = 2;
+let con1 = kiran > rudra;
+let con2 = kiran === rudra;
+
+console.log("both conditation ", (con1 && con2));
+console.log("atleast one conditation ", (con1 || con2));
+console.log("even not any conditation true", !(kiran == rudra));
+
+
+
 
